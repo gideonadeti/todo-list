8191b222a7ad7ea5eb891f65b8815e0bbf0c5b77
Gideon Adeti <gideonadeti0@gmail.com>
2024-05-05 10:07:55 +0000

    Add dummy text to body using DOM manipulation

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 import "./index.css";
+
+const bodyElement = document.querySelector("body");
+
+const h1Element = document.createElement("h1");
+h1Element.textContent = "Todo List";
+
+bodyElement.appendChild(h1Element);
