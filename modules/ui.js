8fb3049e214a50c7ee137661e34675721c0c01be
Gideon Adeti <gideonadeti0@gmail.com>
2024-05-21 17:48:02 +0000

    Refactor ui.js file

--- modules/ui.js
+++ modules/ui.js
@@ -10,96 +10,11 @@ class UI {
     headingDiv.innerHTML = ''
     contentDiv.innerHTML = ''
 
-    const heading = document.createElement('h1')
-    heading.classList.add('project-name')
-    heading.textContent = project.name
-    headingDiv.appendChild(heading)
+    UI.createHeading(headingDiv, project.name)
+    project.todos.forEach((todo) =>
+      UI.createTodoElement(contentDiv, todo, project)
+    )
 
-    project.todos.forEach((todo) => {
-      const todoDiv = document.createElement('div')
-      todoDiv.classList.add('todo')
-
-      const todoTitle = document.createElement('h2')
-      todoTitle.classList.add('title')
-      todoTitle.textContent =
-        todo.title.length > 10 ? todo.title.slice(0, 10) + '...' : todo.title
-
-      const todoDescription = document.createElement('p')
-      todoDescription.textContent =
-        todo.description.length > 30
-          ? todo.description.slice(0, 30) + '...'
-          : todo.description
-
-      const todoDueDate = document.createElement('p')
-      const dueDate = parseISO(todo.dueDate)
-      const formattedDueDate = format(dueDate, "do MMMM',' yyyy")
-      todoDueDate.textContent = formattedDueDate
-
-      const priorityAndParentProjectNameDiv = document.createElement('div')
-      priorityAndParentProjectNameDiv.classList.add(
-        'priority-and-parent-project-name'
-      )
-
-      const todoPriority = document.createElement('p')
-      todoPriority.classList.add('priority', todo.priority)
-      todoPriority.textContent = todo.priority.toUpperCase()
-      priorityAndParentProjectNameDiv.appendChild(todoPriority)
-
-      const todoParentProjectName = document.createElement('p')
-      todoParentProjectName.classList.add('parent-project-name')
-      todoParentProjectName.textContent = todo.parentProjectName
-      priorityAndParentProjectNameDiv.appendChild(todoParentProjectName)
-
-      const controlsDiv = document.createElement('div')
-      controlsDiv.classList.add('controls')
-
-      const todoStatus = document.createElement('input')
-      todoStatus.type = 'checkbox'
-      todoStatus.checked = todo.completed
-      if (todoStatus.checked) {
-        todoDiv.classList.add('completed')
-      }
-      controlsDiv.appendChild(todoStatus)
-
-      todoStatus.addEventListener('change', () => {
-        Store.handleTodoStatusChange(todo.id, todo.parentProjectId)
-
-        const updatedProject = Store.getProject(project.id)
-        this.displayTodos(updatedProject)
-      })
-
-      const editOrViewTodoIconAndDeleteTodoIconDiv =
-        document.createElement('div')
-
-      const editOrViewTodoIcon = document.createElement('span')
-      editOrViewTodoIcon.classList.add('mdi', 'mdi-pencil-outline')
-      editOrViewTodoIconAndDeleteTodoIconDiv.appendChild(editOrViewTodoIcon)
-
-      editOrViewTodoIcon.addEventListener('click', () => {
-        Store.handleTodoModification(todo.id, todo.parentProjectId, project.id)
-      })
-
-      const deleteTodoIcon = document.createElement('span')
-      deleteTodoIcon.classList.add('mdi', 'mdi-delete-outline')
-      editOrViewTodoIconAndDeleteTodoIconDiv.appendChild(deleteTodoIcon)
-
-      deleteTodoIcon.addEventListener('click', () => {
-        Store.removeTodoFromProject(todo.id, todo.parentProjectId)
-
-        const updatedProject = Store.getProject(project.id)
-        this.displayTodos(updatedProject)
-      })
-
-      controlsDiv.appendChild(editOrViewTodoIconAndDeleteTodoIconDiv)
-
-      todoDiv.appendChild(todoTitle)
-      todoDiv.appendChild(todoDescription)
-      todoDiv.appendChild(todoDueDate)
-      todoDiv.appendChild(priorityAndParentProjectNameDiv)
-      todoDiv.appendChild(controlsDiv)
-
-      contentDiv.appendChild(todoDiv)
-    })
     containerDiv.appendChild(headingDiv)
     containerDiv.appendChild(contentDiv)
   }
@@ -112,89 +27,180 @@ class UI {
     headingDiv.innerHTML = ''
     contentDiv.innerHTML = ''
 
-    const heading = document.createElement('h1')
-    heading.textContent = 'My Projects'
-    headingDiv.appendChild(heading)
-
+    UI.createHeading(headingDiv, 'My Projects')
     const projects = Store.getProjects().filter((project) => project.id !== 0)
 
-    projects.forEach((project) => {
-      const numOfTodos = project.todos.length
-      const numOfCompletedTodos = project.todos.filter(
-        (todo) => todo.completed
-      ).length
-      const numOfIncompleteTodos = numOfTodos - numOfCompletedTodos
-
-      const projectDiv = document.createElement('div')
-      projectDiv.classList.add('project')
-
-      const projectName = document.createElement('h2')
-      projectName.classList.add('name')
-      projectName.textContent =
-        project.name.length > 10
-          ? project.name.slice(0, 10) + '...'
-          : project.name
-
-      const numOfTodosP = document.createElement('p')
-      numOfTodosP.classList.add('todos')
-      numOfTodosP.textContent =
-        numOfTodos > 1 ? `${numOfTodos} todos` : `${numOfTodos} todo`
-
-      const numOfIncompleteTodosP = document.createElement('p')
-      numOfIncompleteTodosP.classList.add('incomplete-todos')
-      numOfIncompleteTodosP.textContent = `${numOfIncompleteTodos} incomplete`
-
-      const numOfCompletedTodosP = document.createElement('p')
-      numOfCompletedTodosP.classList.add('completed-todos')
-      numOfCompletedTodosP.textContent = `${numOfCompletedTodos} completed`
-
-      const controlsDiv = document.createElement('div')
-      controlsDiv.classList.add('controls')
-
-      const viewDiv = document.createElement('div')
-      viewDiv.classList.add('mdi', 'mdi-eye-outline')
-      controlsDiv.appendChild(viewDiv)
-
-      viewDiv.addEventListener('click', () => {
-        this.displayTodos(project)
-      })
-
-      const editAndDeleteProjectIconsDiv = document.createElement('div')
-
-      const editProjectIcon = document.createElement('span')
-      editProjectIcon.classList.add('mdi', 'mdi-pencil-outline')
-      editAndDeleteProjectIconsDiv.appendChild(editProjectIcon)
-
-      editProjectIcon.addEventListener('click', () => {
-        if (project.id !== 1) {
-          Store.handleProjectModification(project.id)
-        }
-      })
-
-      const deleteProjectIcon = document.createElement('span')
-      deleteProjectIcon.classList.add('mdi', 'mdi-delete-outline')
-      editAndDeleteProjectIconsDiv.appendChild(deleteProjectIcon)
-
-      deleteProjectIcon.addEventListener('click', () => {
-        if (project.id !== 1) {
-          Store.handleProjectDeletion(project.id)
-          this.displayProjects()
-        }
-      })
-
-      controlsDiv.appendChild(editAndDeleteProjectIconsDiv)
-
-      projectDiv.appendChild(projectName)
-      projectDiv.appendChild(numOfTodosP)
-      projectDiv.appendChild(numOfIncompleteTodosP)
-      projectDiv.appendChild(numOfCompletedTodosP)
-      projectDiv.appendChild(controlsDiv)
-
-      contentDiv.appendChild(projectDiv)
-    })
+    projects.forEach((project) => UI.createProjectElement(contentDiv, project))
 
     containerDiv.appendChild(headingDiv)
     containerDiv.appendChild(contentDiv)
+  }
+
+  static createHeading (container, text) {
+    const heading = document.createElement('h1')
+    if (text !== 'My Projects') {
+      heading.classList.add('project-name')
+    }
+    heading.textContent = text
+    container.appendChild(heading)
+  }
+
+  static createTodoElement (container, todo, project) {
+    const todoDiv = document.createElement('div')
+    todoDiv.classList.add('todo')
+
+    todoDiv.appendChild(
+      UI.createElement('h2', 'title', UI.truncateText(todo.title, 15))
+    )
+    todoDiv.appendChild(
+      UI.createElement('p', '', UI.truncateText(todo.description, 30))
+    )
+    todoDiv.appendChild(UI.createElement('p', '', UI.formatDate(todo.dueDate)))
+
+    const priorityAndParentProjectNameDiv = document.createElement('div')
+    priorityAndParentProjectNameDiv.classList.add(
+      'priority-and-parent-project-name'
+    )
+    priorityAndParentProjectNameDiv.appendChild(
+      UI.createElement(
+        'p',
+        `priority ${todo.priority}`,
+        todo.priority.toUpperCase()
+      )
+    )
+    priorityAndParentProjectNameDiv.appendChild(
+      UI.createElement('p', 'parent-project-name', todo.parentProjectName)
+    )
+
+    const controlsDiv = document.createElement('div')
+    controlsDiv.classList.add('controls')
+    const todoStatus = UI.createTodoStatusCheckbox(todo, project)
+    controlsDiv.appendChild(todoStatus)
+
+    const iconsDiv = UI.createTodoIcons(todo, project)
+    controlsDiv.appendChild(iconsDiv)
+
+    todoDiv.appendChild(priorityAndParentProjectNameDiv)
+    todoDiv.appendChild(controlsDiv)
+
+    if (todoStatus.checked) {
+      todoDiv.classList.add('completed')
+    }
+
+    container.appendChild(todoDiv)
+  }
+
+  static createProjectElement (container, project) {
+    const projectDiv = document.createElement('div')
+    projectDiv.classList.add('project')
+
+    projectDiv.appendChild(
+      UI.createElement('h2', 'name', UI.truncateText(project.name, 15))
+    )
+    projectDiv.appendChild(
+      UI.createElement(
+        'p',
+        'todos',
+        `${project.todos.length} todo${project.todos.length > 1 ? 's' : ''}`
+      )
+    )
+    projectDiv.appendChild(
+      UI.createElement(
+        'p',
+        'incomplete-todos',
+        `${project.todos.filter((todo) => !todo.completed).length} incomplete`
+      )
+    )
+    projectDiv.appendChild(
+      UI.createElement(
+        'p',
+        'completed-todos',
+        `${project.todos.filter((todo) => todo.completed).length} completed`
+      )
+    )
+
+    const controlsDiv = document.createElement('div')
+    controlsDiv.classList.add('controls')
+
+    const viewDiv = UI.createElement('div', 'mdi mdi-eye-outline', '')
+    viewDiv.addEventListener('click', () => UI.displayTodos(project))
+    controlsDiv.appendChild(viewDiv)
+
+    const editAndDeleteIconsDiv = UI.createProjectIcons(project)
+    controlsDiv.appendChild(editAndDeleteIconsDiv)
+
+    projectDiv.appendChild(controlsDiv)
+    container.appendChild(projectDiv)
+  }
+
+  static createElement (tag, className, textContent) {
+    const element = document.createElement(tag)
+    if (className) element.classList.add(...className.split(' '))
+    element.textContent = textContent
+    return element
+  }
+
+  static truncateText (text, length) {
+    return text.length > length ? text.slice(0, length) + '...' : text
+  }
+
+  static formatDate (dateString) {
+    const date = parseISO(dateString)
+    return format(date, "do MMMM',' yyyy")
+  }
+
+  static createTodoStatusCheckbox (todo, project) {
+    const todoStatus = document.createElement('input')
+    todoStatus.type = 'checkbox'
+    todoStatus.checked = todo.completed
+
+    todoStatus.addEventListener('change', () => {
+      Store.handleTodoStatusChange(todo.id, todo.parentProjectId)
+      UI.displayTodos(Store.getProject(project.id))
+    })
+    return todoStatus
+  }
+
+  static createTodoIcons (todo, project) {
+    const iconsDiv = document.createElement('div')
+
+    const editIcon = UI.createElement('span', 'mdi mdi-pencil-outline', '')
+    editIcon.addEventListener('click', () =>
+      Store.handleTodoModification(todo.id, todo.parentProjectId, project.id)
+    )
+    iconsDiv.appendChild(editIcon)
+
+    const deleteIcon = UI.createElement('span', 'mdi mdi-delete-outline', '')
+    deleteIcon.addEventListener('click', () => {
+      Store.removeTodoFromProject(todo.id, todo.parentProjectId)
+      UI.displayTodos(Store.getProject(project.id))
+    })
+    iconsDiv.appendChild(deleteIcon)
+
+    return iconsDiv
+  }
+
+  static createProjectIcons (project) {
+    const iconsDiv = document.createElement('div')
+
+    const editIcon = UI.createElement('span', 'mdi mdi-pencil-outline', '')
+    editIcon.addEventListener('click', () => {
+      if (project.id !== 1) {
+        Store.handleProjectModification(project.id)
+      }
+    })
+    iconsDiv.appendChild(editIcon)
+
+    const deleteIcon = UI.createElement('span', 'mdi mdi-delete-outline', '')
+    deleteIcon.addEventListener('click', () => {
+      if (project.id !== 1) {
+        Store.handleProjectDeletion(project.id)
+        UI.displayProjects()
+      }
+    })
+    iconsDiv.appendChild(deleteIcon)
+
+    return iconsDiv
   }
 }
 
